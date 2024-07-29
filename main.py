3f0d977e4bf9788d6a2f731044e299f875ba4cea
songyejin <yjsong0088@gmail.com>
2024-07-29 11:46:16 +0900

    first

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from domain.answer import answer_router
+from domain.question import question_router
+
+app = FastAPI()
+
+origins = [
+        "http://127.0.0.1:5173", "http://localhost:5173" , 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(question_router.router)
+app.include_router(answer_router.router)
