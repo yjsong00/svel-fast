c923da4722a69abf7f291ee9c6d19f0c37a4aa63
pcmin929 <pcmin929@gmail.com>
2024-07-29 11:46:10 +0900

    first

---
+++ domain/question/question_schema.py
@@ -0,0 +1,29 @@
+import datetime
+
+from pydantic import BaseModel, field_validator
+
+from domain.answer.answer_schema import Answer
+
+
+class Question(BaseModel):
+    id: int
+    subject: str
+    content: str
+    create_date: datetime.datetime
+    answers: list[Answer] = []
+    
+
+class QuestionCreate(BaseModel):
+    subject: str
+    content: str
+
+    @field_validator('subject', 'content')
+    def not_empty(cls, v):
+        if not v or not v.strip():
+            raise ValueError('빈 값은 허용되지 않습니다.')
+        return v
+
+class QuestionList(BaseModel):
+    total: int = 0
+    question_list: list[Question] = []
+    
